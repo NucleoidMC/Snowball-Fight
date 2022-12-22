f305ed80ce9bc806dd1de3b00471688f94a51e15
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-12-22 17:43:49 -0500

    Update minigame to Minecraft 1.19.3

--- src/main/java/io/github/haykam821/snowballfight/Main.java
+++ src/main/java/io/github/haykam821/snowballfight/Main.java
@@ -4,16 +4,16 @@ import io.github.haykam821.snowballfight.game.SnowballFightConfig;
 import io.github.haykam821.snowballfight.game.phase.SnowballFightWaitingPhase;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.Block;
-import net.minecraft.tag.TagKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import xyz.nucleoid.plasmid.game.GameType;
 
 public class Main implements ModInitializer {
 	public static final String MOD_ID = "snowballfight";
 
 	private static final Identifier SNOWBALL_PROVIDERS_ID = new Identifier(MOD_ID, "snowball_providers");
-	public static final TagKey<Block> SNOWBALL_PROVIDERS = TagKey.of(Registry.BLOCK_KEY, SNOWBALL_PROVIDERS_ID);
+	public static final TagKey<Block> SNOWBALL_PROVIDERS = TagKey.of(RegistryKeys.BLOCK, SNOWBALL_PROVIDERS_ID);
 
 	private static final Identifier SNOWBALL_FIGHT_ID = new Identifier(MOD_ID, "snowball_fight");
 	public static final GameType<SnowballFightConfig> SNOWBALL_FIGHT_TYPE = GameType.register(SNOWBALL_FIGHT_ID, SnowballFightConfig.CODEC, SnowballFightWaitingPhase::open);
